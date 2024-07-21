6f6c770a059068d2a67216813958e7c58f8f1ab3
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2024-07-21 10:11:21 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -14,15 +14,37 @@ export default defineConfig({
           name: 'OrganizaTuHorario',
           short_name: 'OrganizaTuHorario',
           description: 'Organiza tu horario de clases de forma sencilla y rápida',
+          id: 'com.santihsilva.organizatuhorario',
+          display: 'fullscreen',
           theme_color: '#000000',
           icons: [
             {
-              src: '/pwa-192x192.png',
+              src: '/icons/android-launchericon-48-48.png',
+              sizes: '48x48',
+              type: 'image/png',
+            },
+            {
+              src: '/icons/android-launchericon-72-72.png',
+              sizes: '72x72',
+              type: 'image/png',
+            },
+            {
+              src: '/icons/android-launchericon-96-96.png',
+              sizes: '96x96',
+              type: 'image/png',
+            },
+            {
+              src: '/icons/android-launchericon-144-144.png',
+              sizes: '144x144',
+              type: 'image/png',
+            },
+            {
+              src: '/icons/android-launchericon-192-192.png',
               sizes: '192x192',
               type: 'image/png',
             },
             {
-              src: '/pwa-512x512.png',
+              src: '/icons/android-launchericon-512-512.png',
               sizes: '512x512',
               type: 'image/png',
             },
