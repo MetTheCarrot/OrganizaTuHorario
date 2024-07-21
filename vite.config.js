4ad8b9ae6cb8219a56378fb86d8d6004f6a5062d
Carrotw <91509157+SantiHSilva@users.noreply.github.com>
2024-07-21 09:55:01 -0500

    add testing pwa vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'OrganizaTuHorario',
+          short_name: 'OrganizaTuHorario',
+          description: 'Organiza tu horario de clases de forma sencilla y rápida',
+          theme_color: '#000000',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+      }
+    )
+  ],
   base: "https://santihsilva.github.io/OrganizaTuHorario/",
 })
